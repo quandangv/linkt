bf4444770aa25ea81c4b823c9511808e6ad3429b
quandangv <quandangv@gmail.com>
2021-01-07 02:06:18 +0700

    Clean up the code Use a single delink function

--- include/document.hpp
+++ include/document.hpp
@@ -9,9 +9,9 @@
 
 namespace lini {
   using std::string;
-
   using errorlist = std::vector<std::pair<int, string>>;
   using str_errlist = std::vector<std::pair<string, string>>;
+
   struct document {
     using sec_map = std::map<string, size_t>;
     using doc_map = std::map<string, sec_map>;
@@ -20,12 +20,10 @@ namespace lini {
     std::vector<string_ref_p> values;
 
     bool add_onetime(const string& section, const string& key, string&& value);
-
     bool has(const string& section, const string& key) const;
     std::optional<size_t> find(const string& section, const string& key) const;
     opt_str get(const string& section, const string& key) const;
     string get(const string& section, const string& key, string&& fallback) const;
     string to_string() const;
   };
-
 }
--- include/string_ref.hpp
+++ include/string_ref.hpp
@@ -8,8 +8,8 @@
 namespace lini {
   using std::string;
   using opt_str = std::optional<string>;
-  class string_ref {
-  public:
+
+  struct string_ref {
     virtual string get() const = 0;
     virtual bool readonly() const { return true; }
     virtual void set(string) {}
@@ -18,58 +18,58 @@ namespace lini {
 
   using string_ref_p = std::unique_ptr<string_ref>;
 
-  class onetime_string : public string_ref {
-  public:
+  struct onetime_string : public string_ref {
     mutable string val;
+
     onetime_string(string&& val) : val(val) {}
     string get() const { return val; }
-    string& get_src() const { return val; }
+    string get_onetime() const { return move(val); }
   };
     
-  class const_string : public string_ref {
-  public:
+  struct const_string : public string_ref {
     string val;
+
     const_string(string&& val) : val(val) {}
     string get() const { return val; }
     bool readonly() const { return false; }
     void set(string value) { val = value; }
   };
 
-  class local_string : public string_ref {
+  struct local_string : public string_ref {
     string_ref_p& ref;
-  public:
+
     local_string(string_ref_p& ref) : ref(ref) {}
     string get() const { return ref->get(); }
     bool readonly() const { return ref->readonly(); }
     void set(string value) { ref->set(value); }
   };
 
-  class fallback_string : public string_ref {
-  public:
+  struct color_string : public string_ref {
+    string_ref_p ref;
+    cspace::processor processor;
+
+    string get() const;
+  };
+
+  struct fallback_string : public string_ref {
     opt_str fallback;
+
     string use_fallback(const string& error_message) const;
   };
 
-  class env_string : public fallback_string {
-  public:
+  struct env_string : public fallback_string {
     string name;
+
     string get() const;
     bool readonly() const;
     void set(string value);
   };
 
-  class file_string : public fallback_string {
-  public:
+  struct file_string : public fallback_string {
     string path;
+
     string get() const;
     bool readonly() const;
     void set(string value);
-  };
-
-  class color_string : public string_ref {
-  public:
-    string_ref_p ref;
-    cspace::processor processor;
-    string get() const;
   };
 }
