3a587a8d8ee9bbb114ba2e1fd5bce0edd93c9edc
quandangv <quandangv@gmail.com>
2021-03-14 22:58:23 +0700

    Example: Improve fps display

--- test/examples/lemonbar.cpp
+++ test/examples/lemonbar.cpp
@@ -3,6 +3,33 @@
 #include <fstream>
 #include <chrono>
 #include <thread>
+#include <sstream>
+#include <cmath>
+
+struct fps_display {
+  double current_sum{0.0}, avg{NAN};;
+  unsigned int count{0}, loop;
+
+  fps_display(unsigned int loop) : loop(loop) {}
+  std::string feed(double value) {
+    std::stringstream ss;
+    current_sum += value;
+    auto current_avg = current_sum / count;
+    if (++count < loop) {
+      if (std::isnan(avg)) {
+        ss << current_avg;
+      } else {
+        auto diff = current_avg - avg;
+        ss << avg << ' ' << std::showpos << diff;
+      }
+    } else {
+      ss << (avg = current_avg);
+      count = 0;
+      current_sum = 0.0;
+    }
+    return ss.str();
+  }
+};
 
 int main() {
   std::ifstream file {"lemonbar.yml"};
@@ -31,19 +58,17 @@ int main() {
 
   std::cout << "If you have lemonbar installed,\n"
       "Pass the result of this program to lemonbar and get a simple status bar\n";
-  double avg_time = 0;
-  constexpr double recent_weight = 0.05;
+  fps_display fps(20);
   while (true) {
     auto next_frame = std::chrono::steady_clock::now() + std::chrono::milliseconds(50);
     try {
       clock_t start_retrieve = clock();
       auto result = wrapper->get_child("lemonbar"_ts);
       double elapsed = double(clock() - start_retrieve) / CLOCKS_PER_SEC * 1000;
-      avg_time = elapsed * recent_weight + avg_time * (1 - recent_weight);
       if (!result)
         std::cout << "Failed to retrieve the key at path 'lemonbar'";
       else
-        std::cout << *result << "%{F#fff B#000} " << avg_time << "ms" << std::endl;
+        std::cout << *result << "%{F#fff B#000} " << fps.feed(elapsed) << "ms" << std::endl;
     } catch (const std::exception& e) {
       std::cout << "Error while retrieving key: " << e.what() << std::endl;
     }
