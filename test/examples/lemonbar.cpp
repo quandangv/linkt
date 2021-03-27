b1bbc50959f65cc5c06bb11d5d86c97c81f234c0
quandangv <quandangv@gmail.com>
2021-03-28 00:59:04 +0700

    Example.lemonbar: Process lemonbar output

--- test/examples/lemonbar.cpp
+++ test/examples/lemonbar.cpp
@@ -50,16 +50,16 @@ int main(int argc, char** argv) {
   file.close();
 
   if (!err.empty()) {
-    std::cout << "Error while parsing:\n";
+    std::cerr << "Error while parsing:\n";
     for (auto& e : err)
-      std::cout << "At " << e.first << ": " << e.second << std::endl;
+      std::cerr << "At " << e.first << ": " << e.second << std::endl;
   }
   node::clone_context context;
   wrapper->optimize(context);
   if (!context.errors.empty()) {
-    std::cout << "Error while optimizing:\n";
+    std::cerr << "Error while optimizing:\n";
     for (auto& e : context.errors)
-      std::cout << "At " << e.first << ": " << e.second << std::endl;
+      std::cerr << "At " << e.first << ": " << e.second << std::endl;
   }
 
   // Fork and call lemonbar
@@ -90,16 +90,35 @@ int main(int argc, char** argv) {
   fps_display fps(40);
   while (true) {
     if (poll(&pollin, 1, 0) > 0 && pollin.revents & POLLIN) {
-      std::string input;
+      char buffer[128];
+      char* buffer_end = buffer;
       while (true) {
-        char lemonbar_output[128];
-        auto count = read(pipes[0], lemonbar_output, 128);
+        auto count = read(pipes[0], buffer, 128);
         if (count == 0) break;
         if (count < 0) return -1;
-        input += lemonbar_output;
-        if (count < 128 || lemonbar_output[127] == '\n') break;
+        buffer_end += count;
+        char* line_start = buffer, *newline;
+        while ((newline = (char*)memchr(line_start, '\n', buffer_end - line_start))) {
+          std::cerr << line_start;
+          *newline = 0;
+          if (!memcmp(line_start, "export ", 7)) {
+            line_start += 7;
+            for (; line_start < newline; line_start++)
+              if (!std::isspace(*line_start))
+                break;
+            auto sep = (char*)memchr(line_start, '=', buffer_end - line_start);
+            if (sep) {
+              *sep = 0;
+              std::cerr << "set " << line_start << " to " << sep + 1 << "\n";
+              setenv(line_start, sep + 1, 1);
+            } else std::cerr << "Invalid: " << line_start << std::endl;
+          } else {
+            system(line_start);
+          }
+          line_start = newline + 1;
+        }
+        if (count < 128 || buffer[127] == '\n') break;
       }
-      std::cout << "Lemonbar says: " << input;
     }
 
     auto next_frame = std::chrono::steady_clock::now() + std::chrono::milliseconds(50);
@@ -108,14 +127,14 @@ int main(int argc, char** argv) {
       auto result = wrapper->get_child("lemonbar"_ts);
       double elapsed = double(clock() - start_retrieve) / CLOCKS_PER_SEC * 1000;
       if (!result)
-        std::cout << "Failed to retrieve the key at path 'lemonbar'";
+        std::cerr << "Failed to retrieve the key at path 'lemonbar'";
       else if (!wrapper->set<float>("lemonbar.fpms.calculation"_ts
           , round(100/fps.feed(elapsed))/100))
-        std::cout << "Can't set fps value";
+        std::cerr << "Can't set fps value";
       fputs(result->data(), lemonbar);
       fflush(lemonbar);
     } catch (const std::exception& e) {
-      std::cout << "Error while retrieving key: " << e.what() << std::endl;
+      std::cerr << "Error while retrieving key: " << e.what() << std::endl;
     }
     std::this_thread::sleep_until(next_frame);
   }
