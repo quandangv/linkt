2856f1a8736dde002598f1c9197992f9fac85882
quandangv <quandangv@gmail.com>
2021-03-31 09:50:12 +0700

    Example.bspwm: center the current desktop

--- test/examples/scripts/bspwm.sh
+++ test/examples/scripts/bspwm.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 while read raw
 do
+  focused=1
   IFS=':' read -r -a array <<< $raw
   for index in {1..10}; do
     case "${array[$index]:0:1}" in
       "f")
-        array[$index]='-'
+        array[$index]='o'
         ;;
       "F")
-        array[$index]='%{T2}+%{T-}'
+        array[$index]='%{T2}O%{T-}'
+        focused=$index
         ;;
       "o")
         array[$index]="${array[$index]:1}"
@@ -17,12 +19,17 @@ do
       "O")
         array[$index]="${array[$index]:1}"
         array[$index]="%{T2}${array[$index]^^}%{T-}"
+        focused=$index
         ;;
     esac
 
     array[$index]="%{A:bspc desktop ^$index -f:}${array[$index]}%{A}"
   done
 
-  echo "%{+u}${array[1]}%{O10}${array[2]}%{O10}${array[3]}%{O10}${array[4]}%{O10}${array[5]}%{O10}${array[6]}%{O10}${array[7]}%{O10}${array[8]}%{O10}${array[9]}%{O10}${array[10]}%{-u}"
+  for index in {1..10}; do
+    result[$((($index-$focused + 15)%10))]=${array[$index]}
+  done
+
+  echo "%{+u}${result[0]}%{O10}${result[1]}%{O10}${result[2]}%{O10}${result[3]}%{O10}${result[4]}%{O10}${result[5]}%{O10}${result[6]}%{O10}${result[7]}%{O10}${result[8]}%{O10}${result[9]}%{-u}"
 done < <(bspc subscribe)
 
