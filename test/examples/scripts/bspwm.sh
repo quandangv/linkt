64e14efc1092f5ecfe49eefcf81b97f3340f7202
quandangv <quandangv@gmail.com>
2021-04-06 12:42:17 +0700

    Examples: Document and add scroll features to bspwm module

--- test/examples/scripts/bspwm.sh
+++ test/examples/scripts/bspwm.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 while read raw; do
   focused=1
-  IFS=':' read -r -a array <<< $raw
+  # Separate raw output into info of individual desktops
+  IFS=':' read -r -a desktop_info <<< $raw
   for index in {1..10}; do
-    case "${array[$index]:0:1}" in
+    dsk_status="${desktop_info[$index]:0:1}"
+    dsk_name="${desktop_info[$index]:1}"
+    # Display each desktop differently based on its status
+    case $dsk_status in
+      # unfocused empty
       "f")
-        array[$index]='o'
-        ;;
+        desktop_info[$index]='o'; ;;
+      # unfocused occupied
       "o")
-        array[$index]="${array[$index]:1}"
-        array[$index]="${array[$index],,}"
-        ;;
-      "O" | "F")
-        array[$index]="${array[$index]:1}"
-        array[$index]="%{T2}${array[$index]^^}%{T-}"
-        focused=$index
-        ;;
+        desktop_info[$index]="${dsk_name,,}"; ;;
+      # focused
+      "O" | "F" | "U")
+        focused=$index; ;&
+      # urgent
+      "u")
+        desktop_info[$index]="%{T2}${dsk_name^^}%{T-}"; ;;
     esac
 
-    array[$index]="%{A:bspc desktop ^$index -f:}${array[$index]}%{A}"
+    # Clicking will switch to that desktop
+    desktop_info[$index]="%{A:bspc desktop ^$index -f: A3:bspc node -d ^$index -f:} ${desktop_info[$index]} %{A A3}"
   done
 
+  # put the focused desktop at the center
   for index in {1..10}; do
-    result[$((($index-$focused + 15)%10))]=${array[$index]}
+    ordered[$((($index-$focused + 15)%10))]=${desktop_info[$index]}
   done
 
-  echo "%{+u +o}${result[0]}%{O10}${result[1]}%{O10}${result[2]}%{O10}${result[3]}%{O10}${result[4]}%{O10}${result[5]}%{O10}${result[6]}%{O10}${result[7]}%{O10}${result[8]}%{O10}${result[9]}%{-u -o}"
+  # Scroll through desktops
+  scroll_cmd="A4:bspc desktop ^$(((focused)%10 + 1)) -f: A5:bspc desktop ^$(((focused + 8)%10 + 1))"
+  echo "%{+u +o $scroll_cmd -f:}${ordered[0]}${ordered[1]}${ordered[2]}${ordered[3]}${ordered[4]}${ordered[5]}${ordered[6]}${ordered[7]}${ordered[8]}${ordered[9]}%{-u A4 -o}"
 done < <(bspc subscribe)
 
